95169c536d7dae2244fd19b3051d7f7a793f0508
Haileamlak Sahle <haileamlaksahle@gmail.com>
2024-07-22 17:07:12 +0300

    updated six files

--- todoApp/src/components/Header.jsx
+++ todoApp/src/components/Header.jsx
@@ -4,6 +4,19 @@ const Header = () => {
   return (
     <div>
         <h1 className={styles.header}>My Todo List</h1>
+        <div className= {styles.textDiv}>
+        <div>
+        <h4>Stay organized and keep track of what needs to be done with our simple and intuitive todo app.</h4>
+        </div>
+        <h5>Welcome! Here's how to use the app</h5>
+        <ul className={styles.olItems}>
+          <li><strong>📝 Add Tasks:</strong> Type your task in the input field and press the 'Add' button.</li>
+          <li><strong>✅ Mark as Done:</strong> Click on a task to mark it as completed.</li>
+          <li><strong>🗑️ Remove Tasks:</strong> Click the 'Delete' button next to a task to remove it from the list.</li>
+          <li><strong>🚮Clear All Tasks:</strong> Click the 'Clear All' button to remove all tasks from the list.</li>
+          <li>📊<strong>Task Count:</strong> The number of completed tasks and the total count are displayed at the bottom of the page.</li>
+        </ul>
+        </div>
     </div>
   )
 }
--- todoApp/src/components/TodoItem.jsx
+++ todoApp/src/components/TodoItem.jsx
@@ -16,7 +16,7 @@ const TodoItem = ({item, todos, setTodos}) => {
         <div className={styles.itemName}>
           <span className={className} onClick={()=>handleClick(item.name)}>{item.name}</span>
           <span>
-            <button onClick={()=>deleteItem(item)} className={styles.deleteBtn}>X</button>
+            <button onClick={()=>deleteItem(item)} className={styles.deleteBtn}>delete</button>
           </span>
         </div>
         <hr className={styles.line}/>
